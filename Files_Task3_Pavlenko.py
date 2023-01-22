d6bab4937619571899f11c494b3bd0caef55f2a3
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-01-22 21:29:00 +0300

    Task3_final

--- Files_Task3_Pavlenko.py
+++ Files_Task3_Pavlenko.py
@@ -1,1 +1,24 @@
 ﻿
+files = {'f1':'1.txt','f2':'2.txt','f3':'3.txt'}
+unsorted_files = {}
+sorted_files = {}
+
+for f in files:
+    with open(files[f], 'r', encoding = 'utf-8') as f:
+        unsorted_files[f.name] = len(f.readlines())
+
+
+
+sorted_list=sorted(unsorted_files.values())
+
+for sorted_value in sorted_list:
+    for key, value in unsorted_files.items():
+        if value==sorted_value:
+            sorted_files[key]=value
+
+for f in sorted_files:
+    with open(f, 'r', encoding = 'utf-8') as current_file:
+        with open ('result.txt', 'a', encoding = 'utf-8') as file_result:
+            file_result.write(f+'\n')
+            file_result.write(str(sorted_files[f])+'\n')
+            file_result.write(current_file.read()+'\n')
